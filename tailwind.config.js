0ffb45cb2dde43e9332129ea28c3dcdf61f3db02
asenataluczak <asenata.luczak@gmail.com>
2024-12-14 19:21:59 +0100

    [styles config] - colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#4ac99b",
+      "primary-selection": "rgba(74, 201, 155, 0.4)",
+      "gray-darker": "#222222",
+      "gray-dark": "#292929",
+      gray: "#383838",
+      "gray-light": "#464646",
+      "gray-lighter": "#a3a3a3",
+      red: "#ff8c67",
+      white: "#cccccc",
+    },
   },
   plugins: [],
-}
-
+};
